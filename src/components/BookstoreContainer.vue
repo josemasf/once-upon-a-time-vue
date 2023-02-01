6dc804b85288faf2f5636193e03fd56815c625f6
Jose Maria Santos <innovation56@iberostar.com>
2023-02-02 00:08:25 +0100

    refactor

--- src/components/BookstoreContainer.vue
+++ src/components/BookstoreContainer.vue
@@ -21,44 +21,6 @@
         class="max-h-[260px] text-clip overflow-hidden"
       />
     </StoryCard>
-
-    <transition name="fade">
-      <div
-        v-if="isModalVisible"
-        class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
-      >
-        <div
-          @click="onToggle"
-          class="absolute bg-black opacity-70 inset-0 z-0 bottom-25"
-        ></div>
-        <div
-          class="w-full p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white dark:bg-gray-800"
-        >
-          <div>
-            <div class="p-3 flex-auto justify-center leading-6">
-              <div
-                class="mb-4 text-lg text-gray-500 dark:text-gray-400 overflow-x-auto max-h-screen"
-              >
-                <div v-html="story"></div>
-              </div>
-            </div>
-            <div class="p-3 mt-2 text-center space-x-4 md:block">
-              <button
-                class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-md hover:shadow-lg hover:bg-gray-100"
-              >
-                Save
-              </button>
-              <button
-                @click="onToggle"
-                class="mb-2 md:mb-0 bg-purple-500 border border-purple-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-md hover:shadow-lg hover:bg-purple-600"
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </transition>
   </div>
 </template>
 
@@ -72,24 +34,7 @@ const mainStore = useMainStore();
 
 const openStoryHandler = (payload: any) => {
   story.value = payload;
-  isModalVisible.value = true;
 };
 
-const isModalVisible = ref(false);
 const story = ref();
-const onToggle = () => {
-  isModalVisible.value = !isModalVisible.value;
-};
 </script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
---
+++ src/components/Molecules/ThePlaces.vue
@@ -0,0 +1,23 @@
+<template>
+  <SelectableContainer :title="'Places'">
+    <AtomAvatar
+      :src="`/imgs/${place.name}.webp`"
+      :alt="place.name"
+      :active="place.active"
+      v-for="place in mainStore.getPlaces().value"
+      @click="clickHandler(place)"
+      :key="place.name"
+    />
+  </SelectableContainer>
+</template>
+
+<script setup lang="ts">
+import { AtomAvatar, SelectableContainer } from "@/components";
+import { useMainStore } from "@/store";
+
+const mainStore = useMainStore();
+
+const clickHandler = (item: any) => {
+  mainStore.toggleLocation(item);
+};
+</script>
