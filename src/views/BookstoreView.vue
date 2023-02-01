049df092555d4f4769c7a1d0ddd6bf6157295984
Jose Maria Santos <innovation56@iberostar.com>
2023-02-02 00:22:40 +0100

    feat empty bookstore

--- src/views/BookstoreView.vue
+++ src/views/BookstoreView.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { BookstoreContainer } from "@/components";
+import { BookstoreContainer, AtomButton } from "@/components";
+import { useMainStore } from "@/store";
+
+const mainStore = useMainStore();
 </script>
 
 <template>
@@ -15,7 +18,21 @@ import { BookstoreContainer } from "@/components";
         bookstore.
       </h1>
     </div>
-    <BookstoreContainer />
+    <BookstoreContainer v-if="mainStore.getStories().value.length > 0" />
+    <main
+      v-else
+      class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]"
+    >
+      <h1 class="text-9xl font-extrabold text-white tracking-widest">
+        bookstore
+      </h1>
+      <div class="bg-[#FF6A3D] px-2 text-3xl rounded rotate-12 absolute">
+        Empty
+      </div>
+      <AtomButton primay class="mt-4">
+        <router-link to="/">Go Home</router-link>
+      </AtomButton>
+    </main>
   </div>
 </template>
 
